bd574d2767ab46375d8c8027b7d4a66be1c4d602
luisfelipej <ljana@saltala.com>
2020-06-10 00:41:08 -0400

    calculo de ponderacion y subida a db

--- soap/utils.php
+++ soap/utils.php
@@ -2,10 +2,26 @@
 
 function insertPostulants($postulants){
   $PDO = new PDO("mysql:host=localhost;dbname=paralela","root","root");
-  $QUERY = $PDO->prepare("INSERT INTO Postulants (rut, nem, ranking, math, lang, cienc, hist) VALUES (?,?,?,?,?,?,?)");
+  $insertPostulantQuery = $PDO->prepare("INSERT INTO Postulants (rut, nem, ranking, math, lang, cienc, hist) VALUES (?,?,?,?,?,?,?)");
+  $getCareersQuery = $PDO->prepare("SELECT id, lang, math, ciencHist, avgMathLang, ranking, nem FROM Careers");
+  $getCareersQuery->execute();
+  $careers = $getCareersQuery->fetchAll();
 
   foreach ($postulants as $key => $scores) {
     $decodedLine = explode(";",$scores);
-    $QUERY->execute($decodedLine);
+    calculatePonderation($decodedLine, $careers, $PDO);
+    $insertPostulantQuery->execute($decodedLine);
+  }
+}
+
+function calculatePonderation($scores, $careers, $PDO) {
+  $insertPostulationQuery = $PDO->prepare("INSERT INTO Postulations (postulantId, careerId, score) VALUES (?,?,?)");
+  foreach ($careers as $career) {
+    $cienc = $career[ciencHist]/100 * $scores[5]; 
+    $hist = $career[ciencHist]/100 * $scores[6];
+    $ciencHist = $cienc > $hist ? $cienc : $hist;
+    $ponderation = $career[lang]/100 * $scores[4] + $career[math]/100 * $scores[3] + 
+      $ciencHist + $career[ranking]/100 * $score[2] + $career[nem]/100 * $score[1];
+    $insertPostulationQuery->execute(array(0 => $scores[0], 1 => $career[0], 2 => $ponderation));
   }
 }
