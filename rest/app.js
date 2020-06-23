1ae52d428bc315342e8d9eb67dad27c3817c419f
luisfelipej <ljana@saltala.com>
2020-06-23 17:21:34 -0400

    config inicial rest

---
+++ rest/app.js
@@ -0,0 +1,20 @@
+const express = require(`express`)
+const cookieParser = require(`cookie-parser`)
+const logger = require(`morgan`)
+const jwt = require(`express-jwt`)
+const cors = require(`cors`)
+
+const indexRouter = require(`./routes/index`)
+
+const app = express()
+
+app.use(cors())
+app.use(jwt({ secret: process.env.SECRET || `secret_token` }))
+app.use(logger(`dev`))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(`/`, indexRouter)
+
+module.exports = app
