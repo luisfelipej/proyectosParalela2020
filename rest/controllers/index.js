7c3cecf906550fa31bf06e9a02e3aaa916298602
Luis Felipe Jaña <luisfelipej@MacBook-Pro-de-Luis.local>
2020-07-13 23:00:36 -0400

    script sync, rutas y controladores necesarios

--- rest/controllers/index.js
+++ rest/controllers/index.js
@@ -1,28 +1,19 @@
 const models = require(`../models`)
 
-async function insertPostulants(postulants) {
-  const postulantPromises = []
-  postulants.forEach((postulant) => {
-    if (postulant.length > 0) {
-      const [rut, nem, ranking, math, lang, cienc, hist] = postulant.split(`;`)
-      postulantPromises.push(
-        models.Postulant.create({ rut, nem, ranking, math, lang, cienc, hist }),
-      )
-    }
-  })
-  await Promise.all(postulantPromises)
-}
+async function insertPostulant(postulant) {}
 
-async function managePostulation(req, res) {
-  const { file } = req.body
+async function careerByCode(req, res) {
+  const { code } = req.query
   try {
-    const buff = Buffer.from(file, `base64`)
-    const data = buff.toString(`ascii`)
-    const postulants = data.split(`\n`)
-    await insertPostulants(postulants)
+    const career = await models.Career.findOne({ where: { code } })
+    if (!career) {
+      return res
+        .status(400)
+        .json({ success: false, error: `Carrera ingresada no existe` })
+    }
     return res.status(200).json({
       success: true,
-      data: postulants,
+      data: career,
     })
   } catch (error) {
     return res.status(500).json({
@@ -32,4 +23,63 @@ async function managePostulation(req, res) {
   }
 }
 
-module.exports = { managePostulation }
+async function careersByName(req, res) {
+  // TODO: normalizar el string de los nombres antes de comprar en la db
+  const { careerNames } = req.query
+  try {
+    const careers = await models.Career.findAll({
+      where: { name: careerNames },
+    })
+    if (careers.length === 0) {
+      return res.status(400).json({
+        success: false,
+        error: `No se han encontrado carreras con el nombre especificado`,
+      })
+    }
+    return res.status(200).json({
+      success: true,
+      data: careers,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+    })
+  }
+}
+
+async function topCareers(req, res) {
+  // TODO: normalizar el string de los nombres antes de comprar en la db
+  const { nem, ranking, math, lang, cienc, hist } = req.body
+  try {
+    const careers = []
+    const postulant = await models.Postulant.findOne({
+      where: {
+        nem,
+        ranking,
+        math,
+        lang,
+        cienc,
+        hist,
+      },
+    })
+    if (postulant) {
+      // Traer sus mejores 10 carreras
+    } else {
+      // Insertar postulante
+      // Calcular ponderaciones en carreras
+      // Seleccionar las mejores 10
+    }
+    return res.status(200).json({
+      success: true,
+      data: careers,
+    })
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error,
+    })
+  }
+}
+
+module.exports = { careerByCode, careersByName, topCareers }
