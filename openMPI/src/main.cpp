f8db771cde56dd3df73dbead524b3093a8b19f8b
Risuxc <jose.carvallom@utem.cl>
2020-08-09 18:03:33 -0400

    Se comenta parte del codigo y se hace un pequeño arreglo en la función GaussianBlur

--- openMPI/src/main.cpp
+++ openMPI/src/main.cpp
@@ -22,35 +22,39 @@ const int MAXBYTES=8*1920*1920;
 uchar buffer[MAXBYTES];
 
 int main(int argc, char *argv[]) {
-
+	// Si no tiene los parametros
     if (argc < 2) {
         cout << "Parametros no validos" << endl;
         return 0;
     }
+    // Se cargan variables necesarias para MPI
     int rank;
     int size;
     ostringstream s;
-
+	// Obtengo el valor solicitado
     int option = stoi(argv[1]);
-
+	// Proceso dicho valor, si no es valido, termino.
     if (option < 1 || option > 3) {
         cout << "Opcion ingresada no valida" << endl;
         return -1;
     }
-
-    // Initializes the MPI execution environment
+    // Una vez validado comenzamos las llamadas MPI
     MPI_Init(&argc, &argv);
+    // Obtenemos rango y numero de procesos
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-
+	// Obtenemos la ruta de nuestra imagen
     string imgPath = argv[2];
+    // Hago lectura del archivo
     Mat img = imread(imgPath, 1);
+    // Si no es valido, desplegar y termino.
     if (!img.data || img.empty()) {
         cout << "No se pudo leer la imagen" << endl;
         return -1;
     }
+    // Divido la imagen en base a la cantidad de hilos disponibles.
     int sliceLength = img.cols/(size-1);
-
+	// Si soy el hilo 0, repartir el trabajo
     if (rank == 0) {
         Mat finalImg;
         int idx = 1;
@@ -72,7 +76,7 @@ int main(int argc, char *argv[]) {
 
     }
 
-    // Terminate MPI execution environment
+    // Termino de trabajar
     MPI_Finalize();
 }
 
@@ -104,17 +108,22 @@ Mat recvMat(int source) {
     return Mat(rows, cols, type, (uchar*)&buffer[3*sizeof(int)]);
 }
 
+// Proceso la imagen segun las opciones solicitadas.
 Mat transformImgByOption(int option, Mat m) {
+	// Creo la imagen final
     Mat finalImg;
+    // Se genera el difuminado mediante Gaussian
     if (option == 1) {
-        GaussianBlur(m, finalImg, Size(m.cols, m.rows), 0);
+        GaussianBlur(m, finalImg, Size(5, 5), 0);
     }
+    // Se amplifica la foto en un 33,34%
     if (option == 3) {
         resize(m, finalImg, Size(m.cols*1.334, m.rows*1.334), 0.334, 0.334);
     }
     return finalImg;
 }
 
+// Funcion que nos permite combinar las imagenes
 void mergeImage(Mat m, Mat & final) {
     if (final.empty()) {
         final = m.clone();
