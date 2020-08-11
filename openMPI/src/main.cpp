63d4dbacf0135dc8e9cfbeec6dc40ccaf6dddbc9
Risuxc <jose.carvallom@utem.cl>
2020-08-10 23:28:46 -0400

    Se utiliza un blur mas adecuado.

--- openMPI/src/main.cpp
+++ openMPI/src/main.cpp
@@ -139,7 +139,7 @@ Mat transformImgByOption(int option, Mat m) {
     Mat finalImg;
     // Se genera el difuminado mediante Gaussian
     if (option == 1) {
-        GaussianBlur(m, finalImg, Size(9, 9), 0);
+        blur(m, finalImg, Size(3, 3),Point(-1,-1));
     }
     if (option == 2) {
         cvtColor(m, finalImg, COLOR_RGB2GRAY);
